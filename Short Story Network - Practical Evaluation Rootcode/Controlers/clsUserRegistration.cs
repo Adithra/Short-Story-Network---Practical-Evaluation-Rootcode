a0b7f1565f857dbfd6120a2cade592f0925ae855
Himesh Perera <himesh@wow.com>
2022-07-23 09:58:31 +0530

    HF

---
+++ Short Story Network - Practical Evaluation Rootcode/Controlers/clsUserRegistration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Short_Story_Network___Practical_Evaluation_Rootcode.Controlers
+{
+    public class clsUserRegistration
+    {
+        #region "CRUD"  
+        public ClientResponse Check_User_ID_Availability(string ID)
+        {
+            List<UserInfo> result;
+            try
+            {
+                using (var ctx = new ShortStoryNetworkContext())
+                {
+                    var rol = UserRoles.writers.ToString();
+                    result = ctx.UserInfoes
+                        .Where(re => re.UserId == ID)
+                        .ToList();
+                }
+                return new ClientResponse { Message = "", State = result.Count >0?true:false, ResultObject = result };
+            }
+            catch (Exception ex)
+            {
+                return new ClientResponse { ClientException = ex };
+            }
+        }
+    }
+}
