1edda6d956641b57054c412bbbff30b4448e5c2b
Himesh Perera <himesh@wow.com>
2022-07-23 19:37:12 +0530

    Image

--- Short Story Network - Practical Evaluation Rootcode/Views/uiNewPost.cs
+++ Short Story Network - Practical Evaluation Rootcode/Views/uiNewPost.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -122,19 +123,48 @@ namespace Short_Story_Network___Practical_Evaluation_Rootcode.Views
                 throw;
             }
         }
+        public byte[] ItemImage = null;
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                panel2.Visible = true;
-                OpenFileDialog open = new OpenFileDialog();
-                open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
-                if (open.ShowDialog() == DialogResult.OK)
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "";
+
+                ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+                string sep = string.Empty;
+
+                dlg.Filter = String.Format("{0}{1}{2} ({3})|{3}", dlg.Filter, sep, "Image files", "*.JPEG;*.JPG;*.JFIF;*.JPEG2000;*.Exif;*.TIFF;*.GIF;*.*.BMP;*.PNG;*.PPM; .PGM; .PBM; *.PNM;*.WebP;*.HEIF;*.BAT;*.BPG;");
+
+                dlg.DefaultExt = ".jpg"; // Default file extension 
+
+                // Show open file dialog box 
+                dlg.ShowDialog();
+
+                string fileName = dlg.FileName;
+
+                if (fileName == "" || fileName == null)
                 {
-                    pictureBox1.Image = new Bitmap(open.FileName);
-                    button1.Tag = open.FileName;
                 }
+                else
+                {
+                    if (ValidFile(dlg.FileName, 102400))
+                    {
+                        pictureBox1.Image = Image.FromFile(fileName);
+                        ItemImage = null;
+                    }
+                    else
+                    {
+                        uPicBox.Image = Image.FromStream(CompreddedImageToByteArray(Image.FromFile(fileName), 20));
+                        //Image img = (Image)uPicBox.Image;
+                        ItemImage = null;
+                        //img.Save(@"C:\Users\SPIL\OneDrive\Documents\Web developments\Downloads\new.jpeg");
+                        //MessageBox.Show("Please select an image less than 1MB");
+                    }
+
+                }
+
             }
             catch (Exception)
             {
@@ -142,5 +172,91 @@ namespace Short_Story_Network___Practical_Evaluation_Rootcode.Views
                 throw;
             }
         }
+
+
+        private byte[] ImageHandler(string fileName)
+        {
+            byte[] ImageData = System.IO.File.ReadAllBytes(fileName);
+            return ImageData;
+
+
+        }
+
+        public byte[] ImageToByteArray(System.Drawing.Image image)
+        {
+            ImageFormat format = image.RawFormat;
+            ImageCodecInfo codec = ImageCodecInfo.GetImageDecoders().First(c => c.FormatID == format.Guid);
+            string mimeType = codec.MimeType;
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, image.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        private bool ValidFile(string filename, long limitInBytes)
+        {
+            var fileSizeInBytes = new FileInfo(filename).Length;
+            if (fileSizeInBytes > limitInBytes)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary> 
+        /// Saves an image as a jpeg image, with the given quality 
+        /// </summary> 
+        /// <param name="path"> Path to which the image would be saved. </param> 
+        /// <param name="quality"> An integer from 0 to 100, with 100 being the highest quality. </param> 
+        public MemoryStream CompreddedImageToByteArray(Image img, int quality)
+        {
+            if (quality < 0 || quality > 100)
+                throw new ArgumentOutOfRangeException("quality must be between 0 and 100.");
+
+            // Encoder parameter for image quality 
+            EncoderParameter qualityParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            // JPEG image codec 
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+            var ms = new MemoryStream();
+            img.Save(ms, jpegCodec, encoderParams);
+            return ms;
+
+        }
+
+        /// <summary> 
+        /// Returns the image codec with the given mime type 
+        /// </summary> 
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        }
+
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            MemoryStream mStream = new MemoryStream();
+            Bitmap bm = null;
+            if (blob.Length > 64)
+            {
+                byte[] pData = blob;
+                mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+                bm = new Bitmap(mStream, false);
+                mStream.Dispose();
+            }
+            return bm;
+        }
+
     }
 }
