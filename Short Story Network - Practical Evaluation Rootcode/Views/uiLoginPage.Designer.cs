4b87aedd31d23e905fbd370ca3d796111686e9f0
Himesh Perera <himesh@wow.com>
2022-07-23 08:48:52 +0530

    Login

--- Short Story Network - Practical Evaluation Rootcode/Views/uiLoginPage.Designer.cs
+++ Short Story Network - Practical Evaluation Rootcode/Views/uiLoginPage.Designer.cs
@@ -28,19 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.userNameText = new System.Windows.Forms.TextBox();
+            this.PassowrdText = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // userNameText
+            // 
+            this.userNameText.Location = new System.Drawing.Point(30, 40);
+            this.userNameText.Name = "userNameText";
+            this.userNameText.Size = new System.Drawing.Size(255, 20);
+            this.userNameText.TabIndex = 0;
+            // 
+            // PassowrdText
+            // 
+            this.PassowrdText.Location = new System.Drawing.Point(30, 112);
+            this.PassowrdText.Name = "PassowrdText";
+            this.PassowrdText.Size = new System.Drawing.Size(255, 20);
+            this.PassowrdText.TabIndex = 0;
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.DodgerBlue;
+            this.login.FlatAppearance.BorderSize = 0;
+            this.login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login.ForeColor = System.Drawing.Color.White;
+            this.login.Location = new System.Drawing.Point(210, 151);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 1;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Passowrd";
             // 
             // uiLoginPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(322, 196);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.PassowrdText);
+            this.Controls.Add(this.userNameText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "uiLoginPage";
             this.Text = "Login page";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox userNameText;
+        private System.Windows.Forms.TextBox PassowrdText;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
